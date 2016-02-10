99e467296e43854561b97b51727f3e7f9bc1b112
Greg Holt <gregory_holt@icloud.com>
2016-02-10 08:56:31 -0600

    Experimenting with the speed of various methods
    $ go test -bench=. -benchtime=10s
    BenchmarkDebugPrintf-8  10000000          2184 ns/op
    BenchmarkDebugPrintf2-8 10000000          1497 ns/op
    BenchmarkDebugPrintf3-8 10000000          1839 ns/op
    BenchmarkDebugPrintf4-8 10000000          1838 ns/op
    BenchmarkDebugPrintf5-8 10000000          1274 ns/op
    BenchmarkDebugPrintf6-8 10000000          1254 ns/op
    BenchmarkDebugPrintf7-8 10000000          1252 ns/op
    BenchmarkDebugPrintf8-8 10000000          1282 ns/op
    
    Types 5-8 are all pretty close.
    
    I'm not sure we really want 7 or 8, as they'd keep the buffer at
    whatever size it grows to -- one huge message and it's around from then
    on; whereas 5 and 6 will just keep the 1k buffer at most.
    
    Between 5 and 6, I guess we'll take 6 as it's so slightly faster. But
    maybe we'll switch to 5 if we determine 6 is creating to much garbage to
    be collected.

--- simplelogger.go
+++ simplelogger.go
@@ -1,6 +1,7 @@
 package simplelogger
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"sync"
@@ -11,6 +12,7 @@ type SimpleLogger struct {
 	lock sync.Mutex
 	out  io.Writer
 	err  io.Writer
+	buf  []byte
 
 	criticalPrefixFmt string
 	errorPrefixFmt    string
@@ -22,6 +24,7 @@ func New(name string, out io.Writer, err io.Writer) *SimpleLogger {
 		out:               out,
 		err:               err,
 		criticalPrefixFmt: fmt.Sprintf("2006-01-02 15:05:05 CRITICAL %s ", name),
+		buf:               make([]byte, 1024),
 		errorPrefixFmt:    fmt.Sprintf("2006-01-02 15:05:05 ERROR %s ", name),
 		debugPrefixFmt:    fmt.Sprintf("2006-01-02 15:05:05 DEBUG %s ", name),
 	}
@@ -45,6 +48,48 @@ func (s *SimpleLogger) DebugPrintf(msg string, args ...interface{}) {
 	s.lock.Unlock()
 }
 
+func (s *SimpleLogger) DebugPrintf2(msg string, args ...interface{}) {
+	s.lock.Lock()
+	simpleLoggerPrintf2(s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf3(msg string, args ...interface{}) {
+	s.lock.Lock()
+	simpleLoggerPrintf3(s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf4(msg string, args ...interface{}) {
+	s.lock.Lock()
+	simpleLoggerPrintf4(s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf5(msg string, args ...interface{}) {
+	s.lock.Lock()
+	simpleLoggerPrintf5(s.buf, s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf6(msg string, args ...interface{}) {
+	s.lock.Lock()
+	simpleLoggerPrintf6(s.buf, s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf7(msg string, args ...interface{}) {
+	s.lock.Lock()
+	s.simpleLoggerPrintf7(s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
+func (s *SimpleLogger) DebugPrintf8(msg string, args ...interface{}) {
+	s.lock.Lock()
+	s.simpleLoggerPrintf8(s.out, s.debugPrefixFmt, msg, args...)
+	s.lock.Unlock()
+}
+
 func simpleLoggerPrintf(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
 	out.Write([]byte(time.Now().Format(prefixFmt)))
 	m := fmt.Sprintf(msg, args...)
@@ -52,4 +97,71 @@ func simpleLoggerPrintf(out io.Writer, prefixFmt string, msg string, args ...int
 	if len(m) < 1 || m[len(m)-1] != '\n' {
 		out.Write([]byte("\n"))
 	}
+}
+
+func simpleLoggerPrintf2(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	m := time.Now().Format(prefixFmt) + fmt.Sprintf(msg, args...)
+	if len(m) == 0 || m[len(m)-1] != '\n' {
+		m += "\n"
+	}
+	out.Write([]byte(m))
+}
+
+func simpleLoggerPrintf3(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	m := time.Now().Format(prefixFmt) + fmt.Sprintf(msg, args...)
+	out.Write([]byte(m))
+	if len(m) == 0 || m[len(m)-1] != '\n' {
+		out.Write([]byte("\n"))
+	}
+}
+
+func simpleLoggerPrintf4(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	out.Write([]byte(time.Now().Format(prefixFmt)))
+	m := fmt.Sprintf(msg, args...)
+	if len(m) == 0 || m[len(m)-1] != '\n' {
+		m += "\n"
+	}
+	out.Write([]byte(m))
+}
+
+func simpleLoggerPrintf5(buf []byte, out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	buf = time.Now().AppendFormat(buf[:0], prefixFmt)
+	buf = append(buf, []byte(fmt.Sprintf(msg, args...))...)
+	if len(buf) == 0 || buf[len(buf)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+	out.Write(buf)
+}
+
+func simpleLoggerPrintf6(buf []byte, out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	buf = time.Now().AppendFormat(buf[:0], prefixFmt)
+	bufr := bytes.NewBuffer(buf)
+	fmt.Fprintf(bufr, msg, args...)
+	buf = bufr.Bytes()
+	if len(buf) == 0 || buf[len(buf)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+	out.Write(buf)
+}
+
+func (s *SimpleLogger) simpleLoggerPrintf7(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	buf := time.Now().AppendFormat(s.buf[:0], prefixFmt)
+	bufr := bytes.NewBuffer(buf)
+	fmt.Fprintf(bufr, msg, args...)
+	buf = bufr.Bytes()
+	if len(buf) == 0 || buf[len(buf)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+	out.Write(buf)
+	s.buf = buf
+}
+
+func (s *SimpleLogger) simpleLoggerPrintf8(out io.Writer, prefixFmt string, msg string, args ...interface{}) {
+	buf := time.Now().AppendFormat(s.buf[:0], prefixFmt)
+	buf = append(buf, []byte(fmt.Sprintf(msg, args...))...)
+	if len(buf) == 0 || buf[len(buf)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+	out.Write(buf)
+	s.buf = buf
 }
